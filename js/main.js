99cc59e3f058f7268c45b62a58295503a47c4fac
jauzou <jauzou@qq.com>
2017-10-12 18:27:10 +0800

    add music

--- js/main.js
+++ js/main.js
@@ -18,4 +18,41 @@ $(function(){
    		var s = 'slider slider' + ($(this).index()+1);
    		slider.attr('class', s);
   	});
+
+  	var sound = new Howl({
+	  src: ["images/show.mp3"],
+	  autoplay: true,
+	  loop: true,
+	  // volume: 0.5,
+	});
+
+	var aa = 1;
+		$('.music').hover(function(){
+			if (aa==1) {
+				$(this).find('span').eq(0).css('display','block');
+			}else  if(aa==0){
+				$(this).find('span').eq(1).css('display','block');
+			}	
+		},function(){
+			$(this).find('span').hide();
+		})
+
+
+		$('.music').on('click',function(){
+
+			if ($(this).find('span').eq(0).css('display')=='block') {
+				aa=0;
+				$(this).find('span').eq(0).css('display','none');
+				$(this).find('span').eq(1).css('display','block');
+				sound.pause();
+				return aa;
+			}else {
+				aa=1;
+				$(this).find('span').eq(0).css('display','block');
+				$(this).find('span').eq(1).css('display','none');
+				sound.play();
+				return aa;
+			}
+			$(this).find('span').hide();
+		});
 })
